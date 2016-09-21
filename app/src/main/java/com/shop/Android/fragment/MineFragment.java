a42912f2246555acac1fb2e154e45d54ea50ac94
tbb_d6g <1091117506@qq.com>
2016-09-21 10:24:10 +0800

    all

--- app/src/main/java/com/shop/Android/fragment/MineFragment.java
+++ app/src/main/java/com/shop/Android/fragment/MineFragment.java
@@ -1,10 +1,13 @@
 package com.shop.Android.fragment;
 
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.shop.Android.activity.MineEvaluateActivity;
 import com.shop.Android.activity.MsgActivity;
 import com.shop.Android.base.BaseActvity;
 import com.shop.Android.base.BaseFragment;
+import com.shop.Android.widget.MineView;
 import com.shop.R;
 
 /**
@@ -15,6 +18,15 @@ public class MineFragment extends BaseFragment {
     private String TAG = "mine";
     private ImageView mLeftIv;
     private ImageView mIconIv;
+    private MineView mEvaluateMv;
+    private MineView mCollectMv;
+    private MineView mIntegralMv;
+    private MineView mAddressMv;
+    private MineView mShareMv;
+    private MineView mOpinionMv;
+    private MineView mHelpMv;
+    private MineView mSetMv;
+    private LinearLayout mContactLl;
 
     @Override
     protected int loadLayout() {
@@ -24,7 +36,7 @@ public class MineFragment extends BaseFragment {
     @Override
     protected void init() {
         F();
-        setOnClicks(mLeftIv, mIconIv);
+        setOnClicks(mLeftIv, mIconIv,mAddressMv,mCollectMv,mContactLl,mIntegralMv,mEvaluateMv,mHelpMv,mOpinionMv,mSetMv,mShareMv);
     }
 
 
@@ -35,6 +47,25 @@ public class MineFragment extends BaseFragment {
                 openActivity(MsgActivity.class);
                 break;
             case R.id.ft_mine_icon_iv:
+                break;
+            case R.id.ft_mine_address_mv:
+                break;
+            case R.id.ft_mine_collect_mv:
+                break;
+            case R.id.ft_mine_contact_ll:
+                break;
+            case R.id.ft_mine_evaluate_mv:
+                openActivity(MineEvaluateActivity.class);
+                break;
+            case R.id.ft_mine_help_mv:
+                break;
+            case R.id.ft_mine_integral_mv:
+                break;
+            case R.id.ft_mine_opinion_mv:
+                break;
+            case R.id.ft_mine_set_mv:
+                break;
+            case R.id.ft_mine_share_mv:
                 break;
         }
 
