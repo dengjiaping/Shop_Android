723a1ad06aa43abf3166187152e4774d907e211e
tbb_d6g <1091117506@qq.com>
2016-09-29 09:38:21 +0800

    all

---
+++ app/src/main/java/com/shop/Android/activity/OrderSearchActivity.java
@@ -0,0 +1,24 @@
+package com.shop.Android.activity;
+
+import com.shop.Android.base.BaseActvity;
+import com.shop.R;
+
+/**
+ * Created by admin on 2016/9/28.
+ */
+public class OrderSearchActivity extends BaseActvity {
+    @Override
+    protected int loadLayout() {
+        return R.layout.activity_order_search;
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected void onClickSet(int i) {
+
+    }
+}
