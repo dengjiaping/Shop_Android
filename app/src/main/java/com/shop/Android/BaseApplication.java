10ea21e2ceefff11e2955b4c0a76467d8c0723b8
renwanqiu <1342330295@qq.com>
2016-09-29 09:24:55 +0800

    1.修改

--- app/src/main/java/com/shop/Android/BaseApplication.java
+++ app/src/main/java/com/shop/Android/BaseApplication.java
@@ -1,5 +1,6 @@
 package com.shop.Android;
 
+import com.king.Internet.user_method.CallServer;
 import com.king.KingApplication;
 import com.king.KingConfig;
 import com.shop.R;
@@ -12,6 +13,7 @@ public class BaseApplication extends KingApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        CallServer.BASE_URL = "http://eshop.ittapp.com/api.php/%s";
         KingConfig.mDefaultImage = R.drawable.default_image;
         KingConfig.mCircleDefaultImage = R.drawable.default_image;
     }
