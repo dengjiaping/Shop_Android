68366c1d0a1b728118733198d580ede4c8b6505c
tbb_d6g <1091117506@qq.com>
2016-10-10 17:29:30 +0800

    all

--- app/src/main/java/com/shop/Net/Param/MessageParam.java
+++ app/src/main/java/com/shop/Net/Param/MessageParam.java
@@ -4,7 +4,7 @@ package com.shop.Net.Param;
  * Created by admin on 2016/10/10.
  */
 public class MessageParam  {
-    private String token;
+    private String token ;
 
     public MessageParam(String token) {
         this.token = token;
